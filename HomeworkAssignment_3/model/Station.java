1a4ff575620944b4d82baf0ea704ae93f0f84129
keya-college <keya.shah001@stud.fh-dortmund.de>
2025-11-06 21:13:00 +0100

    editing station class

--- HomeworkAssignment_3/model/Station.java
+++ HomeworkAssignment_3/model/Station.java
@@ -21,26 +21,3 @@ public class Station {
     public int getId() { return id; }
 }
 
-package HomeworkAssignment_3.model;
-
-public class Station {
-    private final int id;
-    private boolean occupied;
-
-    public Station(int id) {
-        this.id = id;
-    }
-
-    public synchronized boolean occupy() {
-        if (occupied) return false;
-        occupied = true;
-        return true;
-    }
-
-    public synchronized void release() {
-        occupied = false;
-    }
-
-    public int getId() { return id; }
-}
-
